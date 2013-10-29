b91105971c6bca2737b36230876320a0f7d074d2
Monte Lunacek <monte.lunacek@colorado.edu>
2013-10-29 12:49:58 -0600

    Update functions.h

--- lab/lab-08-integrate/functions.h
+++ lab/lab-08-integrate/functions.h
@@ -1,20 +1,25 @@
 #include <math.h>
+#include <unistd.h>
 
 
 double func_a(const double x){
-	const double y = x-10;
-	return 0.25*y*y*y + 0.75*y*y - 1.5*y + 8.0;
+	usleep(40000);
+    const double y = x-10;
+    return 0.25*y*y*y + 0.75*y*y - 1.5*y + 8.0;
 }
 
 double func_b(const double x){
-	return 10000.0*exp(-1.0*x)/x;
+	usleep(40000);
+    return 10000.0*exp(-1.0*x)/x;
 }
 
 double func_c(const double x){
-	return sin(x)*x;
+	usleep(40000);
+    return sin(x)*x;
 }
 
 double func_d(const double x){
-	const double y = x-7.5;
-	return y/sqrt(1+y*y) + 2.0;
+	usleep(40000);
+    const double y = x-7.5;
+    return y/sqrt(1+y*y) + 2.0;
 }
