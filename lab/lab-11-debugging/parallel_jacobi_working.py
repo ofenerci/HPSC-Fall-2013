07bef3f1bb14b619dbdab0a27360ae9d88245821
mlunacek <monte.lunacek@gmail.com>
2013-11-13 08:45:48 -0700

    work from class

--- lab/lab-11-debugging/parallel_jacobi_working.py
+++ lab/lab-11-debugging/parallel_jacobi_working.py
@@ -81,11 +81,45 @@ class ParallelJacobi:
  
     # Gather methods
     def gather_matrix(self):
-        pass
-    
+        if self.c_rank == 0:
+            # recv
+            N = int(math.sqrt(self.size))
+            for i in range(N):
+                for j in range(N):
+                    from_rank = self.c_comm.Get_cart_rank([i,j])
+                    #print i, j, from_rank
+                    if from_rank != self.c_rank:
+                        buff = self.recv_results(from_rank)
+                        #print 'recv from ', from_rank, buff
+                        self.copy_to_matrix(buff, from_rank, i, j)
+        else:
+            self.send_result()
+
+    def copy_to_matrix(self, buff, from_rank, i, j):
+        nx = self.sub_n - 2
+        start_row = i*nx + 1
+        start_col = j*nx + 1
+        end_row = start_row + nx
+        end_col = start_col + nx
+        print start_row, end_row, start_col, end_col, i, j
+
+        self.A[start_row:end_row, start_col:end_col] = buff[1:nx+1,1:nx+1]
+
+    def send_result(self):
+        self.c_comm.Send([self.sub_A, MPI.DOUBLE], 0, tag=12)
+
+    def recv_results(self, from_rank):
+        buff = np.zeros(self.sub_A.shape)
+        self.c_comm.Recv([buff, MPI.DOUBLE], from_rank, tag=12)
+        return buff
+
     # Test these methods
     def test_gather(self):
-        pass
+        B = self.sub_A.copy()
+        self.sub_A[:] = self.c_rank
+        self.gather_matrix()
+        self.printstate()
+        self.sub_A = B
 
     def test_shift(self):
         pass
